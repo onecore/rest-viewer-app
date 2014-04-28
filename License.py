8f73e29d1d694ff35ce99715349c6d6ef3b48f8c
Mark Anthony Pequeras <onecore@live.com>
2014-04-27 21:52:57 -0700

    Create License.py
    I know licenses are useless for the skids. :) jk,.

---
+++ License.py
@@ -0,0 +1,106 @@
+# CoreSEC Softwares License
+from bz2 import __author__
+
+
+class License:
+    """
+    CoreSEC Softwares Agreement and The Python Software Foundation License (Version 2)
+    
+    call function CoreSEC() for doc
+    call function Software() for doc
+    """
+    
+    def CoreSEC(self):
+        """
+        This Agreement is solely given to CoreSEC Under the full Python Software Foundation License (Version 2)
+        """
+        def __init__(self):
+            self.__License__ = """
+    ------------------------------------------------------------------
+    CoreSEC Softwares LICENSE AGREEMENT FOR PYTHON 0.9.0 THROUGH 3
+    ------------------------------------------------------------------
+    
+    Copyright (c) 2011 - 2014, CoreSEC Softwares , The Philippines / Canada. All rights reserved.
+    
+    Permission to use, and distribute this software and its documentation for any purpose and without fee is hereby granted, 
+    provided that the above copyright notice appear in all copies and that both that copyright notice and this permission notice appear in supporting documentation,
+    and that the name of CoreSEC Softwares or CoreSEC not be used in advertising or publicity pertaining to distribution of the software without specific,
+    written prior permission.
+    
+    CoreSEC Softwares or "CoreSEC" DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS,
+    IN NO EVENT SHALL CoreSEC Softwares or "CoreSEC" BE LIABLE FOR ANY SPECIAL,
+    INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+    NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+    """
+
+
+
+        
+    def Software(self):
+        """
+        This License is Between the CoreSEC Software's Project (Open/Closed Source)
+        Under the Python Software Foundation License (Version 2)
+        """
+        def __init__(self):
+            
+            self.__License__ = """
+    --------------------------------------------
+    PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+    --------------------------------------------
+    
+    1. This LICENSE AGREEMENT is between the CoreSEC Softwares (CORESEC)
+    ,and the Individual or Organization ("Licensee") accessing and
+    otherwise using this software ("CoreSEC Projects") in source or binary form and
+    its associated documentation.
+    
+    2. Subject to the terms and conditions of this License Agreement, CORESEC
+    hereby grants Licensee a nonexclusive, royalty-free, world-wide
+    license to reproduce, analyze, test, perform and/or display publicly,
+    prepare derivative works, distribute, and otherwise use CoreSEC Projects
+    alone or in any derivative version, provided, however, that CORESEC
+    License Agreement and PSF's notice of copyright, i.e., "Copyright (c)
+    2001, 2002, 2003, 2004, 2005, 2006 Python Software Foundation; All Rights
+    Reserved" are retained in Python alone or in any derivative version
+    prepared by Licensee.
+    
+    3. In the event Licensee prepares a derivative work that is based on
+    or incorporates CoreSEC Projects or any part thereof, and wants to make
+    the derivative work available to others as provided herein, then
+    Licensee hereby agrees to include in any such work a brief summary of
+    the changes made to CoreSEC Projects.
+    
+    4. PSF is making Python available to Licensee on an "AS IS"
+    basis. PSF MAKES NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR
+    IMPLIED. BY WAY OF EXAMPLE, BUT NOT LIMITATION, PSF MAKES NO AND
+    DISCLAIMS ANY REPRESENTATION OR WARRANTY OF MERCHANTABILITY OR FITNESS
+    FOR ANY PARTICULAR PURPOSE OR THAT THE USE OF CoreSEC Projects WILL NOT
+    INFRINGE ANY THIRD PARTY RIGHTS.
+    
+    5. CoreSEC SHALL NOT BE LIABLE TO LICENSEE OR ANY OTHER USERS OF CoreSEC Projects
+    FOR ANY INCIDENTAL, SPECIAL, OR CONSEQUENTIAL DAMAGES OR LOSS AS
+    A RESULT OF MODIFYING, DISTRIBUTING, OR OTHERWISE USING CoreSEC Projects,
+    OR ANY DERIVATIVE THEREOF, EVEN IF ADVISED OF THE POSSIBILITY THEREOF.
+    
+    6. This License Agreement will automatically terminate upon a material
+    breach of its terms and conditions.
+    
+    7. Nothing in this License Agreement shall be deemed to create any
+    relationship of agency, partnership, or joint venture between CoreSEC and
+    Licensee. This License Agreement does not grant permission to use PSF
+    trademarks or trade name in a trademark sense to endorse or promote
+    products or services of Licensee, or any third party.
+    
+    8. By copying, installing or otherwise using Python, Licensee
+    agrees to be bound by the terms and conditions of this License
+    Agreement.
+    """
+
+
+    def Contributions(self):
+        def __init__(self):
+            __developers__ = ["Mark Anthony Pequeras",
+                              "Jay Paterno",
+                              "Maria Shiela Magistrado",
+                              "Tony Birol"]
+            __company__ = ["CoreSEC Softwares (Canada/Philippines)",
+                           "Geek Talks Philippines"]
